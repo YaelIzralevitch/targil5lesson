8965e2798b4cf8a0cb3fa6e0e6ef12b5c6fb6b88
Yael <Yael@DESKTOP-G993II2>
2024-01-30 19:13:55 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void redLight_Click(object sender, RoutedEventArgs e)
+        {
+            if (errorImage.IsVisible)
+            {
+                errorImage.Visibility = Visibility.Hidden;
+            }
+            MessageBox.Show("BREAKS");
+        }
+
+        public int i = 0;
+
+        private void greenLight_Click(object sender, RoutedEventArgs e)
+        {
+            i = i + 12;
+            pinkCar.Margin = new Thickness(603 - i, 252, 0, 0);
+        }
+
+        private void yellowLight_Click(object sender, RoutedEventArgs e)
+        {
+            i = i + 5;
+            pinkCar.Margin = new Thickness(603 - i, 252, 0, 0);
+            errorImage.Visibility = Visibility.Visible;   
+        }
+    }
+}
